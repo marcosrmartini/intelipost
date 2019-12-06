876d325ebab8bf8449cc35e3509068e1ceb5780b
Marcos Roberto Martini <mmartini@br.ibm.com>
2019-12-06 12:00:15 -0300

    Config

--- src/main/java/br/com/intelipost/model/Artista.java
+++ src/main/java/br/com/intelipost/model/Artista.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 
 import br.com.intelipost.payload.ArtistaPayload;
 
