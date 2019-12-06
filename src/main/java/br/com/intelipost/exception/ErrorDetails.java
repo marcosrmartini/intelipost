ad56af18e131ce5675b6a6a5be1e9a42808b1b6c
Marcos Martini <marcosrmartini@gmail.com>
2019-12-06 15:54:46 -0300

    Tratamento de mensage

--- src/main/java/br/com/intelipost/exception/ErrorDetails.java
+++ src/main/java/br/com/intelipost/exception/ErrorDetails.java
@@ -1,34 +1,35 @@
 package br.com.intelipost.exception;
 
-
 import java.util.Date;
 
 public class ErrorDetails {
-    private Date timestamp;
-    private String message;
-    private String details;
+	private Date timestamp;
+	private String message;
+	private String details;
 
-    public ErrorDetails(Date timestamp, String message, String details) {
-        super();
-        this.timestamp = timestamp;
-        this.message = message; //this.pegarSomenteAMensagem(message);
-        this.details = details;
-    }
+	public ErrorDetails(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = this.pegarSomenteAMensagem(message);
+		this.details = details;
+	}
 
-    private String pegarSomenteAMensagem(String msg) {
-   	String sa = msg.substring(msg.indexOf("default message ["));
-    	return sa;
-    }
-    
-    public Date getTimestamp() {
-        return timestamp;
-    }
+	private String pegarSomenteAMensagem(String msg) {
+		String sa = msg.substring(msg.indexOf("default message ["));
+		sa = sa.replaceAll("default message", "");
+		sa = sa.replaceAll("]]", "]");
+		return sa;
+	}
 
-    public String getMessage() {
-         return message;
-    }
+	public Date getTimestamp() {
+		return timestamp;
+	}
 
-    public String getDetails() {
-         return details;
-    }
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
 }
